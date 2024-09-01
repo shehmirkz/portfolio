e1b520cd2f255b8bd77df49d8fb1fa6891fa7b76
shehmir hussain <shahmir.hussain.khanzada@gmail.com>
2024-09-01 23:22:44 +0500

    fix: alignmenmt (wip)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
-import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { TextGenerateEffect } from "./ui/Text-generate-effect";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
     return (
 
-        <div className="pb-20 pt-36">
+        <div className="pb-20 pt-15">
             <div>
                 <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
                 <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
@@ -15,7 +15,7 @@ const Hero = () => {
             </div>
 
 
-            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute flex items-center justify-center top-0 left-0">
+            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center relative top-0 left-0">
 
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100  bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
 
