be89fe020cb30fb44078eaf3d6bf1482c9a3591c
shehmir hussain <shahmir.hussain.khanzada@gmail.com>
2024-09-23 11:27:48 +0500

    feat: testimo section (wip)

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
+import { testimonials } from '@/data'
 
 const Clients = () => {
     return (
-        <div>Clients</div>
+        <div className='py-20' id='projects'>
+            <h1 className='heading'>
+                Kind words from
+                <span className='text-purple'> Satisfied Clients</span>
+            </h1>
+            <div className='flex flex-col items-center'>
+                <InfiniteMovingCards
+                    items={testimonials}
+                    direction='right'
+                    speed='slow'
+
+                />
+            </div>
+        </div>
     )
 }
 
