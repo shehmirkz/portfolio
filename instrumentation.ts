f1340918f4a92c7d1fbbf7db1f3fe9a8a9875c82
shehmir hussain <shahmir.hussain.khanzada@gmail.com>
2026-01-23 23:36:04 +0500

    chore: polyfill for three-globe

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,32 @@
 import * as Sentry from '@sentry/nextjs';
 
+// Set up document polyfill for three-globe during SSR
+if (typeof globalThis !== 'undefined' && typeof globalThis.document === 'undefined') {
+  globalThis.document = {
+    createElement: () => ({
+      setAttribute: () => {},
+      getAttribute: () => null,
+      appendChild: () => {},
+      removeChild: () => {},
+      style: {},
+      innerHTML: '',
+      className: '',
+    }),
+    createElementNS: function(ns: string, tagName: string) {
+      return this.createElement(tagName);
+    },
+    body: {
+      appendChild: () => {},
+      removeChild: () => {},
+      style: {},
+    },
+  } as any;
+}
+
+if (typeof global !== 'undefined' && typeof (global as any).document === 'undefined') {
+  (global as any).document = globalThis.document;
+}
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./sentry.server.config');
