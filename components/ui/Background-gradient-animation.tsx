a6c59109785d16b32e25fb0f2aba1e424fddfd6a
shehmir hussain <shahmir.hussain.khanzada@gmail.com>
2026-01-23 23:36:39 +0500

    refactor: background gradient animation

--- components/ui/Background-gradient-animation.tsx
+++ components/ui/Background-gradient-animation.tsx
@@ -42,7 +42,9 @@ export const BackgroundGradientAnimation = ({
     const [isSafari, setIsSafari] = useState(false);
 
     useEffect(() => {
-        setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
+        if (typeof navigator !== 'undefined') {
+            setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
+        }
         setIsMounted(true); // Mark component as mounted
     }, []);
 
